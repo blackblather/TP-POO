1d16695068f5afd9bfd8824284bdd8d05c1cbb85
FaceRAX <33099221+FaceRAX@users.noreply.github.com>
2018-01-11 07:35:50 +0000

    stuff

--- Main/Formiga.h
+++ Main/Formiga.h
@@ -2,22 +2,23 @@
 #include "Elemento.h"
 
 class Ninho;
+class Migalha;
 class Formiga : public Elemento
 {
 	static int N_formiga;
-	int ID_formiga, raioVisao = 3;
+	int ID_formiga, raioVisao = 3, raioMovimento=5;
 	bool is_in_nest;
 	Ninho* motherLand;
 	bool RegraFoge(char** arrMapa, vector<Ninho>* pNinhos);
-	bool RegraPersegue(char** arrMapa);
+	bool RegraPersegue(char** arrMapa, vector<Ninho>* pNinhos);
 	bool RegraAssalta(char** arrMapa);
-	bool RegraProtege(char** arrMapa);
+	bool RegraProtege(char** arrMapa, vector<Ninho>* pNinhos);
 	bool RegraProcuraMigalha(char** arrMapa);
-	bool RegraComeMigalha(char** arrMapa);
+	bool RegraComeMigalha(char** arrMapa, vector<Migalha>* pMigalhas);
 	bool RegraVaiParaNinho(char** arrMapa);
 	bool RegraPasseia(char** arrMapa);
 public:
-	void ActionFormiga(char** arrMapa, vector<Ninho>* pNinhos);
+	void ActionFormiga(char** arrMapa, vector<Ninho>* pNinhos, vector<Migalha>* pMigalhas);
 	posXY GetPosFormiga() const;
 	void GetAntInfo() const;
 	Formiga(posXY pos, char type, Ninho* ninho);
